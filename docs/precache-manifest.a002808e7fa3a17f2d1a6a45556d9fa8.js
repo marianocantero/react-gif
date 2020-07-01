a6a074a8488d2b125094d934478780d7971037eb
marianocantero <marianojoaquinc@gmail.com>
2020-07-01 13:14:06 -0300

    Docs agregado

---
+++ docs/precache-manifest.a002808e7fa3a17f2d1a6a45556d9fa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b05d340d8e72425306909c1d011e52c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e40c97f119c38702033d",
+    "url": "/static/css/main.e3ab755f.chunk.css"
+  },
+  {
+    "revision": "e0e2e837c3a98b8bee87",
+    "url": "/static/js/2.12041336.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.12041336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e40c97f119c38702033d",
+    "url": "/static/js/main.76073107.chunk.js"
+  },
+  {
+    "revision": "8fbd23898c71323fe1aa",
+    "url": "/static/js/runtime-main.4c8be365.js"
+  }
+]);
